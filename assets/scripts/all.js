865e93d83207f696d68351cf42740fa0d38899e7
Erica <erica@Ericade-iMac.local>
2021-03-18 22:57:34 +0800

    Update 2021-03-18T14:57:33.598Z

--- assets/scripts/all.js
+++ assets/scripts/all.js
@@ -1,239 +1,236 @@
-'use strict'
+"use strict";
 
-const getElemt = (elemt) => document.querySelector(elemt)
+var getElemt = function getElemt(elemt) {
+  return document.querySelector(elemt);
+};
 
-const height = getElemt('#height')
-const weight = getElemt('#weight')
-const calcBtn = getElemt('#btn-calculator')
-const result = getElemt('#result')
-const BMI = getElemt('.res-num')
-const resMsg = getElemt('.res-msg')
-const renew = getElemt('#btn-result')
-const records = getElemt('.listBox')
-const deleteBtn = getElemt('#deletBtn')
-const inputs = document.querySelectorAll('.inputBox input')
-const save = JSON.parse(localStorage.getItem('record')) || []
-let tag = ''
-let entKey = 0
+var height = getElemt('#height');
+var weight = getElemt('#weight');
+var calcBtn = getElemt('#btn-calculator');
+var result = getElemt('#result');
+var BMI = getElemt('.res-num');
+var resMsg = getElemt('.res-msg');
+var renew = getElemt('#btn-result');
+var records = getElemt('.listBox');
+var deleteBtn = getElemt('#deletBtn');
+var inputs = document.querySelectorAll('.inputBox input');
+var save = JSON.parse(localStorage.getItem('record')) || [];
+var tag = '';
+var entKey = 0; 
 
 function update(data) {
-	let cont = ''
-	let newData = JSON.parse(JSON.stringify(data))
-	newData.reverse()
+  var cont = '';
+  var newData = JSON.parse(JSON.stringify(data));
+  newData.reverse();
 
-	for (let i = 0; i < newData.length; i++) {
-		cont += '<li data-list='
-			.concat(i, '>\n\t\t\t<div class="list-msg">\n\t\t\t\t<span class="tag ')
-			.concat(newData[i].tag, '"></span>\n\t\t\t\t<span class="txt">')
-			.concat(
-				newData[i].txt,
-				'</span>\n\t\t\t</div>\n\t\t\t<div class="list-bmi">',
-			)
-			.concat(newData[i].bmi, '</div>\n\t\t\t<div class="list-weight">')
-			.concat(newData[i].weight, 'kg</div>\n\t\t\t<div class="list-height">')
-			.concat(newData[i].height, 'cm</div>\n\t\t\t<div class="list-date">')
-			.concat(
-				newData[i].date,
-				'</div>\n\t\t\t<i class="material-icons delete"  data-clear="',
-			)
-			.concat(i, '">delete</i>\n\t\t</li>')
-	}
+  for (var i = 0; i < newData.length; i++) {
+    cont += "<li data-list=".concat(i, ">\n      <div class=\"list-msg\">\n        <span class=\"tag ").concat(newData[i].tag, "\"></span>\n        <span class=\"txt\">").concat(newData[i].txt, "</span>\n      </div>\n      <div class=\"list-bmi\">").concat(newData[i].bmi, "</div>\n      <div class=\"list-weight\">").concat(newData[i].weight, "kg</div>\n      <div class=\"list-height\">").concat(newData[i].height, "cm</div>\n      <div class=\"list-date\">").concat(newData[i].date, "</div>\n      <i class=\"material-icons delete\"  data-clear=\"").concat(i, "\">delete</i>\n    </li>");
+  }
 
-	if (save.length >= 1) {
-		records.innerHTML = cont
-		deleteBtn.style.display = 'block'
-	} else {
-		records.innerHTML = '還沒有BMI紀錄，快來面對現實吧！'
-		deleteBtn.style.display = 'none'
-	}
-}
+  if (save.length >= 1) {
+    records.innerHTML = cont;
+    deleteBtn.style.display = 'block';
+  } else {
+    records.innerHTML = '還沒有BMI紀錄，快來面對現實吧！';
+    deleteBtn.style.display = 'none';
+  }
+} 
+
 
 function handleStatus(data) {
-	const status = {
-		thin: {
-			color: '#31BAF9',
-			tag: 'thin',
-			msg: '過輕',
-		},
-		normal: {
-			color: '#86D73F',
-			tag: 'normal',
-			msg: '理想',
-		},
-		overweight: {
-			color: '#FF982D',
-			tag: 'overweight',
-			msg: '過重',
-		},
-		heavy: {
-			color: '#FF6C03',
-			tag: 'heavy',
-			msg: '輕度肥胖',
-		},
-		fat: {
-			color: '#FF6C03',
-			tag: 'fat',
-			msg: '中度肥胖',
-		},
-		overfat: {
-			color: '#FF1200',
-			tag: 'overfat',
-			msg: '重度肥胖',
-		},
-	}
+  var status = {
+    thin: {
+      color: '#31BAF9',
+      tag: 'thin',
+      msg: '過輕'
+    },
+    normal: {
+      color: '#86D73F',
+      tag: 'normal',
+      msg: '理想'
+    },
+    overweight: {
+      color: '#FF982D',
+      tag: 'overweight',
+      msg: '過重'
+    },
+    heavy: {
+      color: '#FF6C03',
+      tag: 'heavy',
+      msg: '輕度肥胖'
+    },
+    fat: {
+      color: '#FF6C03',
+      tag: 'fat',
+      msg: '中度肥胖'
+    },
+    overfat: {
+      color: '#FF1200',
+      tag: 'overfat',
+      msg: '重度肥胖'
+    }
+  };
 
-	const filterStatus = (value) => {
-		return [
-			changeColor(status[value].color),
-			(tag = status[value].tag),
-			(resMsg.textContent = status[value].msg),
-		]
-	}
+  var filterStatus = function filterStatus(value) {
+    var color = changeColor(status[value].color);
+    resMsg.textContent = status[value].msg;
+    tag = status[value].tag;
+    return {
+      color: color,
+      tag: tag
+    };
+  };
 
-	if (data <= 18.5) {
-		filterStatus('thin')
-	} else if (data <= 25) {
-		filterStatus('normal')
-	} else if (data <= 30) {
-		filterStatus('overweight')
-	} else if (data <= 35) {
-		filterStatus('heavy')
-	} else if (data <= 40) {
-		filterStatus('fat')
-	} else {
-		filterStatus('overfat')
-	}
+  if (data <= 18.5) {
+    filterStatus('thin');
+  } else if (data <= 25) {
+    filterStatus('normal');
+  } else if (data <= 30) {
+    filterStatus('overweight');
+  } else if (data <= 35) {
+    filterStatus('heavy');
+  } else if (data <= 40) {
+    filterStatus('fat');
+  } else {
+    filterStatus('overfat');
+  }
 }
 
 function changeColor(color) {
-	result.style.background = color
-	result.style.color = color
-	renew.style.background = color
-	return color
-}
+  result.style.background = color;
+  result.style.color = color;
+  renew.style.background = color;
+  return color;
+} 
+
 
 function calculate() {
-	let w = weight.value
-	let h = height.value / 100
-	let bmi = Math.round((w / Math.pow(h, 2)) * 100) / 100
-	handleStatus(bmi)
-	BMI.textContent = bmi
-	result.style.display = 'block'
-	calcBtn.style.display = 'none'
-	let newData = {
-		tag: tag,
-		txt: resMsg.textContent,
-		bmi: bmi,
-		weight: weight.value,
-		height: height.value,
-		date: nowDate().date,
-		time: nowDate().time,
-	}
-	save.push(newData)
+  var w = weight.value;
+  var h = height.value / 100;
+  var bmi = Math.round(w / Math.pow(h, 2) * 100) / 100;
+  handleStatus(bmi);
+  BMI.textContent = bmi;
+  result.style.display = 'block';
+  calcBtn.style.display = 'none';
+  var newData = {
+    tag: tag,
+    txt: resMsg.textContent,
+    bmi: bmi,
+    weight: weight.value,
+    height: height.value,
+    date: nowDate().date,
+    time: nowDate().time
+  };
+  save.push(newData); 
 
-	let toString = JSON.stringify(save)
-	localStorage.setItem('record', toString)
-	update(save)
-}
+  var toString = JSON.stringify(save);
+  localStorage.setItem('record', toString);
+  update(save);
+} 
+
 
 function nowDate(value) {
-	let today = new Date()
-	let year = today.getFullYear()
-	let month = (today.getMonth() + 1 < 10 ? '0' : '') + (today.getMonth() + 1)
-	let date = (today.getDate() < 10 ? '0' : '') + today.getDate()
-	let time = today.getTime()
-	value = month + '-' + date + '-' + year
-	return {
-		date: value,
-		time: time,
-	}
-}
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = (today.getMonth() + 1 < 10 ? '0' : '') + (today.getMonth() + 1);
+  var date = (today.getDate() < 10 ? '0' : '') + today.getDate();
+  var time = today.getTime();
+  value = month + '-' + date + '-' + year;
+  return {
+    date: value,
+    time: time
+  };
+} 
+
 
 function blurCheck(data) {
-	const txt = [' 身高', ' 體重']
-	let str = ''
-	let chk = true
-	let regex = /^(0|[1-9][0-9]*)$/
+  var txt = [' 身高', ' 體重'];
+  var str = '';
+  var chk = true;
+  var regex = /^(0|[1-9][0-9]*)$/;
 
-	for (let i = 0; i < data.length; i++) {
-		let intValue = data[i].value.trim()
+  for (var i = 0; i < data.length; i++) {
+    var intValue = data[i].value.trim();
 
-		if (!regex.exec(intValue) || intValue <= 0) {
-			data[i].classList.add('focus')
-			str += txt[i]
-			chk = false
-		} else {
-			data[i].classList.remove('focus')
-		}
-	}
+    if (!regex.exec(intValue) || intValue <= 0) {
+      data[i].classList.add('focus');
+      str += txt[i];
+      chk = false;
+    } else {
+      data[i].classList.remove('focus');
+    }
+  }
 
-	return {
-		msg: str,
-		chk: chk,
-	}
+  return {
+    msg: str,
+    chk: chk
+  };
 }
 
 function checkFun() {
-	if (blurCheck(inputs).chk) {
-		calculate()
-	} else {
-		alert('請輸入您的' + blurCheck(inputs).msg)
-		inputs.forEach(function (item) {
-			item.addEventListener('blur', function () {
-				blurCheck(inputs)
-			})
-		})
-		return
-	}
-}
+  if (blurCheck(inputs).chk) {
+    calculate();
+  } else {
+    alert('請輸入您的' + blurCheck(inputs).msg);
+    inputs.forEach(function (item) {
+      item.addEventListener('blur', function () {
+        blurCheck(inputs);
+      });
+    });
+  }
+} 
+
 
 function renewFun() {
-	height.value = ''
-	weight.value = ''
-	resMsg.textContent = ''
-	result.style.display = 'none'
-	calcBtn.style.display = 'block'
-	inputs[0].focus()
-}
+  height.value = '';
+  weight.value = '';
+  resMsg.textContent = '';
+  result.style.display = 'none';
+  calcBtn.style.display = 'block';
+  inputs[0].focus();
+} 
+
 
 function clearData(e) {
-	let clear = e.target.dataset.clear
-	if (!clear) return
-	save.splice(clear, 1)
-	localStorage.setItem('record', JSON.stringify(save))
-	update(save)
+  var clear = e.target.dataset.clear;
+  if (!clear) return;
+  save.splice(clear, 1);
+  localStorage.setItem('record', JSON.stringify(save));
+  update(save);
 }
 
 function clearAll() {
-	save.splice(0)
-	localStorage.setItem('record', JSON.stringify(save))
-	update(save)
-}
+  save.splice(0);
+  localStorage.setItem('record', JSON.stringify(save));
+  update(save);
+} 
+
 
 function keyCtrl(e) {
-	if (e.keyCode === 13) {
-		let isCalced = calcBtn.style.display === 'block'
-		let isChecked = blurCheck(inputs).chk
+  if (e.keyCode === 13) {
+    var isCalced = calcBtn.style.display === 'block';
+    var isChecked = blurCheck(inputs).chk;
 
-		if (entKey === 1 && !isCalced) {
-			renewFun()
-			entKey = 0
-		} else if (entKey === 0 && !isChecked) {
-			checkFun()
-		} else {
-			checkFun()
-			entKey = 1
-		}
-	} else return
-}
+    if (entKey === 1 && !isCalced) {
+      renewFun();
+      entKey = 0;
+    } else if (entKey === 0 && !isChecked) {
+      checkFun();
+    } else {
+      checkFun();
+      entKey = 1;
+    }
+  } else return;
+} 
+
 
 function init() {
-	calcBtn.addEventListener('click', checkFun)
-	renew.addEventListener('click', renewFun)
-	records.addEventListener('click', clearData)
-	deleteBtn.addEventListener('click', clearAll)
-	window.addEventListener('keydown', keyCtrl)
-	update(save)
+  calcBtn.addEventListener('click', checkFun);
+  renew.addEventListener('click', renewFun);
+  records.addEventListener('click', clearData);
+  deleteBtn.addEventListener('click', clearAll);
+  window.addEventListener('keydown', keyCtrl);
+  update(save);
 }
 
-init()
+init();
